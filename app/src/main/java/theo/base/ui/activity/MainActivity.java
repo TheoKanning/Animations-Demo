89c86b9e72227c66790a10981e11ea54c87f02be
Theo Kanning <theokanning@gmail.com>
2015-12-27 21:46:33 -0600

    First commit

---
+++ app/src/main/java/theo/base/ui/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package theo.base.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import theo.base.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
